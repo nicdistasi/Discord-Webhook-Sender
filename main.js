89088723a8e95c6c621857ddda0f2afa11276cd4
nicdistasi <85811764+nicdistasi@users.noreply.github.com>
2021-12-04 19:03:23 -0800

    Update main.js

--- main.js
+++ main.js
@@ -9,12 +9,16 @@ function sendMessage() {
         username: "Hey Toby ;)",
         content: message
     }
-    request.open("POST", "https://discordapp.com/api/webhooks/916878690602860605/75ObZaLvfGgTuge1B_EIznRS_rA8we_a5dQYaP85w-I70xYq_aoWKJ2vYHhhz7xS67_O");
-    request.setRequestHeader('Content-type', 'application/json');
-    request.send(JSON.stringify(params));
+    if (message != '') {
+        request.open("POST", "https://discordapp.com/api/webhooks/916878690602860605/75ObZaLvfGgTuge1B_EIznRS_rA8we_a5dQYaP85w-I70xYq_aoWKJ2vYHhhz7xS67_O");
+        request.setRequestHeader('Content-type', 'application/json');
+        request.send(JSON.stringify(params));
 
-    info.innerHTML = 'Message sent';
-    textbox.value = '';
+        info.innerHTML = 'Message sent';
+        textbox.value = '';
+    } else {
+        info.innerHTML = 'Invalid message';
+    }
 }
 
 submitBtn.addEventListener('click', sendMessage);

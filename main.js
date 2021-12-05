fce2679ef5266dd5ee84158daf59836196b573d2
nicdistasi <85811764+nicdistasi@users.noreply.github.com>
2021-12-04 21:26:45 -0800

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,9 @@ function sendMessage() {
         content: message
     }
     if (message != '') {
-        request.open("POST", "https://discordapp.com/api/webhooks/916878690602860605/75ObZaLvfGgTuge1B_EIznRS_rA8we_a5dQYaP85w-I70xYq_aoWKJ2vYHhhz7xS67_O");
+        //luc's server: https://discordapp.com/api/webhooks/916889198622023691/7grsd65JWIX6NRclSVW35O8Hvnp9ff8tAHivg1prFw5Z-V7CLfZ95_I6j54jyzuCTdYY
+        //code server: https://discordapp.com/api/webhooks/916878690602860605/75ObZaLvfGgTuge1B_EIznRS_rA8we_a5dQYaP85w-I70xYq_aoWKJ2vYHhhz7xS67_O
+        request.open("POST", "https://discordapp.com/api/webhooks/916889198622023691/7grsd65JWIX6NRclSVW35O8Hvnp9ff8tAHivg1prFw5Z-V7CLfZ95_I6j54jyzuCTdYY");
         request.setRequestHeader('Content-type', 'application/json');
         request.send(JSON.stringify(params));
 

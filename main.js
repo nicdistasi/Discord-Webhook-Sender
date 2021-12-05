fdc9c93d6d2a4c071b82afdb0f676a72a5f6648f
nicdistasi <85811764+nicdistasi@users.noreply.github.com>
2021-12-04 18:59:46 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+const submitBtn = document.getElementById('submitBtn');
+const textbox = document.getElementById('message');
+const info = document.getElementById('infoText');
+
+function sendMessage() {
+    const message = textbox.value;
+    const request = new XMLHttpRequest();
+    const params = {
+        username: "Hey Toby ;)",
+        content: message
+    }
+    request.open("POST", "https://discordapp.com/api/webhooks/916878690602860605/75ObZaLvfGgTuge1B_EIznRS_rA8we_a5dQYaP85w-I70xYq_aoWKJ2vYHhhz7xS67_O");
+    request.setRequestHeader('Content-type', 'application/json');
+    request.send(JSON.stringify(params));
+
+    info.innerHTML = 'Message sent';
+    textbox.value = '';
+}
+
+submitBtn.addEventListener('click', sendMessage);
